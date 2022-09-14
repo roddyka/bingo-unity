8b80e0050ce876904815eddfa534c17644029bbe
rodrigo antunes <rodrigoka@live.com>
2022-09-14 12:29:00 +0100

    creating the basic functions to insert credits and change the texts

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -9,13 +9,17 @@ public class gameController : MonoBehaviour
 {
     public static gameController instance; //instance to call this item on anothers script
     public GameObject[] CanvasItems; //UI objects
+    public TextMeshProUGUI TitleText; //Credit Text
     public TextMeshProUGUI CreditText; //Credit Text
     public TextMeshProUGUI CounterText; //Counter Text
+    public int Credits = 0; //number of credits
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Credits = 0;
+        CreditText.text = "Credits: "+Credits.ToString();
+        TitleText.text = "OH GOD ITS BINGO DAY!";
     }
 
     // Update is called once per frame
@@ -30,6 +34,18 @@ public class gameController : MonoBehaviour
         if(SceneManager.sceneCountInBuildSettings > nextSceneIndex)
         {
             SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    public void InsertCredit(){
+        //inserting credits function
+        Credits++; //adding +1 to credits
+        CreditText.text = "Credits: "+Credits.ToString(); //change the credits text
+        if(Credits > 0){
+            //verify if the credits is avaliable
+            CanvasItems[3].SetActive(false); //Hide insert credit button
+            TitleText.text = "Now its time to generate your card"; //change the title text sending instructions
+            CanvasItems[4].SetActive(true); //active the generate card button
         }
     }
 }
